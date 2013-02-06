2f0d0a4a318635975765237d70c6b8344c80155a
tareqahmed.projects@gmail.com <tareqahmed.projects@gmail.com@efdfb960-d8b1-3b62-e861-4de92b6f2b69>
2013-02-06 17:08:40 +0000

    ~ Tareq Ahmed 06/02/2013 5:06pm
    What's been done so far:
    - Move camera forwards and backwards
    - Strafe camera to the left and right
    
    There is a "flicker" when the camera strafes left or right

--- trunk/Camera.cpp
+++ trunk/Camera.cpp
@@ -10,12 +10,12 @@
 
 CCamera::CCamera(void)
 {
-	m_fSpeed=0;
+	m_fSpeed=2;
 }
 
 CCamera::CCamera(float x_, float y_,float z_):C3dObject(x_,y_,z_)
 {
-	m_fSpeed=0;
+	m_fSpeed=2;
 }
 
 CCamera::~CCamera(void)
@@ -25,32 +25,49 @@ CCamera::~CCamera(void)
 void
 CCamera::Update(float dt_)
 {
-
-
-	//here is an example of how to check for input
-	if (CInput::Instance()->GetIfKeyDown(DIK_A))
-	{
-	//does something->you need to edit this!!
-	}
-
-	//calculate new lookat based on current Yaw,pitch roll
-
 	float cosYaw,sinYaw,sinPitch;
 	cosYaw=(float)cos(DEG2RAD(m_fYaw));
 	sinYaw=(float)sin(DEG2RAD(m_fYaw));
 	sinPitch=(float)sin(DEG2RAD(m_fPitch));
-
-	//update position based on speed
-	//depends on current yaw angle
-
-	m_fX+=sinYaw*m_fSpeed*dt_;
-	m_fZ+=-cosYaw*m_fSpeed*dt_;
 
 	//update lookat based on yaw and pitch
 	float lookAtx=m_fX+sinYaw;
 	float lookAty=m_fY+sinPitch;
 	float lookAtz=m_fZ-cosYaw;
 
+
+	if (CInput::Instance()-> GetIfKeyDown(DIK_W))
+	{	
+		// Move fowards
+		m_fZ -= m_fSpeed *dt_;
+	}
+	else if (CInput::Instance()-> GetIfKeyDown(DIK_S))
+	{	
+		// Move backwards
+		m_fZ += m_fSpeed*dt_;
+	}
+	else if (CInput::Instance()->GetIfKeyDown(DIK_A))
+	{
+		// strafe left
+		m_fX-= m_fSpeed * dt_;
+	}
+	else if (CInput::Instance()->GetIfKeyDown(DIK_D))
+	{	
+		// Strafe right
+		m_fX+= m_fSpeed* dt_;
+	}
+
+
+	//calculate new lookat based on current Yaw,pitch roll
+
+
+
+	//update position based on speed
+	//depends on current yaw angle
+
+	//m_fX+=sinYaw*m_fSpeed*dt_;
+	//m_fZ+=-cosYaw*m_fSpeed*dt_;
+
+
 	gluLookAt(m_fX,m_fY,m_fZ,lookAtx,lookAty,lookAtz,0,1,0);
 }
-
